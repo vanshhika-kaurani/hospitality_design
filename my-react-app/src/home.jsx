ca1f6261f5e5eca063bc3556c16ef69064b091f1
vanshhika-kaurani <140744923+vanshhika-kaurani@users.noreply.github.com>
2025-07-09 06:20:21 +0000

    Added Home page

---
+++ my-react-app/src/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+     <head>
+          
+     </head>
+     
+    </div>
+  );
+};
+
+export default Home;
